b0ac8d9f099545333098bf8025a5f6a87fa463d3
koirand <koirand.jp@gmail.com>
2020-02-08 23:28:06 +0900

    implement authentication with oauth

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/koirand/tweet
 
 go 1.13
+
+require (
+	github.com/fatih/color v1.9.0
+	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,150 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"github.com/fatih/color"
+	"github.com/garyburd/go-oauth/oauth"
+)
+
+func getConfig() (string, map[string]string, error) {
+	dir := os.Getenv("HOME")
+	if dir == "" && runtime.GOOS == "windows" {
+		dir = os.Getenv("APPDATA")
+		if dir == "" {
+			dir = filepath.Join(os.Getenv("USERPROFILE"), "Application Data", "koirand-tweet")
+		}
+		dir = filepath.Join(dir, "koirand-tweet")
+	} else {
+		dir = filepath.Join(dir, ".config", "koirand-tweet")
+	}
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		return "", nil, err
+	}
+
+	var file string
+	file = filepath.Join(dir, "settings.json")
+	config := map[string]string{}
+
+	b, err := ioutil.ReadFile(file)
+	if err != nil && !os.IsNotExist(err) {
+		return "", nil, err
+	}
+	if err != nil {
+		config["ClientToken"] = "AIPf0Bq576tUsyZnPTnVb0th6"
+		config["ClientSecret"] = "d78H90qH7SO1qJgeNPA1SRjfnyHrffrVLwb7NyMsEzFwjJImCe"
+	} else {
+		err = json.Unmarshal(b, &config)
+		if err != nil {
+			return "", nil, fmt.Errorf("could not unmarshal %v: %v", file, err)
+		}
+	}
+	return file, config, nil
+}
+
+var oauthClient = oauth.Client{
+	TemporaryCredentialRequestURI: "https://api.twitter.com/oauth/request_token",
+	ResourceOwnerAuthorizationURI: "https://api.twitter.com/oauth/authenticate",
+	TokenRequestURI:               "https://api.twitter.com/oauth/access_token",
+}
+
+func clientAuth(requestToken *oauth.Credentials) (*oauth.Credentials, error) {
+	var err error
+	browser := "xdg-open"
+	url := oauthClient.AuthorizationURL(requestToken, nil)
+
+	args := []string{url}
+	if runtime.GOOS == "windows" {
+		browser = "rundll32.exe"
+		args = []string{"url.dll,FileProtocolHandler", url}
+	} else if runtime.GOOS == "darwin" {
+		browser = "open"
+		args = []string{url}
+	} else if runtime.GOOS == "plan9" {
+		browser = "plumb"
+	}
+	color.Set(color.FgHiRed)
+	fmt.Println("Open this URL and enter PIN.")
+	color.Set(color.Reset)
+	fmt.Println(url)
+	browser, err = exec.LookPath(browser)
+	if err == nil {
+		cmd := exec.Command(browser, args...)
+		cmd.Stderr = os.Stderr
+		err = cmd.Start()
+		if err != nil {
+			return nil, fmt.Errorf("cannot start command: %v", err)
+		}
+	}
+
+	fmt.Print("PIN: ")
+	stdin := bufio.NewScanner(os.Stdin)
+	if !stdin.Scan() {
+		return nil, fmt.Errorf("canceled")
+	}
+	accessToken, _, err := oauthClient.RequestToken(http.DefaultClient, requestToken, stdin.Text())
+	if err != nil {
+		return nil, fmt.Errorf("cannot request token: %v", err)
+	}
+	return accessToken, nil
+}
+
+func getAccessToken(config map[string]string) (*oauth.Credentials, bool, error) {
+	oauthClient.Credentials.Token = config["ClientToken"]
+	oauthClient.Credentials.Secret = config["ClientSecret"]
+
+	authorized := false
+	var token *oauth.Credentials
+	accessToken, foundToken := config["AccessToken"]
+	accessSecret, foundSecret := config["AccessSecret"]
+	if foundToken && foundSecret {
+		token = &oauth.Credentials{Token: accessToken, Secret: accessSecret}
+	} else {
+		requestToken, err := oauthClient.RequestTemporaryCredentials(http.DefaultClient, "", nil)
+		if err != nil {
+			err = fmt.Errorf("cannot request temporary credentials: %v", err)
+			return nil, false, err
+		}
+		token, err = clientAuth(requestToken)
+		if err != nil {
+			err = fmt.Errorf("cannot request temporary credentials: %v", err)
+			return nil, false, err
+		}
+
+		config["AccessToken"] = token.Token
+		config["AccessSecret"] = token.Secret
+		authorized = true
+	}
+	return token, authorized, nil
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	file, config, err := getConfig()
+	if err != nil {
+		log.Fatalf("cannot get configuration: %v", err)
+	}
+
+	_, authorized, err := getAccessToken(config)
+	if err != nil {
+		log.Fatalf("cannot get access token: %v", err)
+	}
+	if authorized {
+		b, err := json.MarshalIndent(config, "", "  ")
+		if err != nil {
+			log.Fatalf("cannot store file: %v", err)
+		}
+		err = ioutil.WriteFile(file, b, 0700)
+		if err != nil {
+			log.Fatalf("cannot store file: %v", err)
+		}
+	}
 }
